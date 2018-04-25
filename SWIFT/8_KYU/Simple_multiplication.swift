8fab1d4807e12cb11bb2a46ad6fd477446c6bd98
Elijah <gemar19@mail.ru>
2018-04-25 12:22:17 +0300

    swift code

---
+++ SWIFT/8_KYU/Simple_multiplication.swift
@@ -0,0 +1,15 @@
+func simpleMultiplication(_ num: Int) -> Int
+{
+if num % 2 == 0 
+{
+return num * 8
+}
+else 
+{
+return num * 9
+}
+}
+
+//func simpleMultiplication(_ num: Int) -> Int
+{
+if num % 2 == 0?num*8:num*9}
