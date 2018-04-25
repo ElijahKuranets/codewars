b4bb59a8c5d8a09614166a0ddda30127b87618c5
ElijahKuranets <gemar19@mail.ru>
2018-04-25 19:22:17 +0300

    Update Simple_multiplication.swift

--- SWIFT/8_KYU/Simple_multiplication.swift
+++ SWIFT/8_KYU/Simple_multiplication.swift
@@ -10,6 +10,4 @@ return num * 9
 }
 }
 
-//func simpleMultiplication(_ num: Int) -> Int
-{
-if num % 2 == 0?num*8:num*9}
+//func simpleMultiplication(_ num: Int) -> Int{ if num % 2 == 0?return num*8:return num*9 }
